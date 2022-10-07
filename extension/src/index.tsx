649776fd5a3b5546dce3ecf1a892d7926ad3d77f
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2022-10-07 19:07:08 +0530

    use official ts template

--- extension/src/index.tsx
+++ extension/src/index.tsx
@@ -1,26 +1,17 @@
-import { enableAllPlugins } from 'immer';
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './assets/scss/index.scss';
-import { App } from './components/App/App';
-import { ErrorBoundary } from './components/ErrorBoundary/ErrorBoundary';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-enableAllPlugins();
-
-const root = createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </StrictMode>
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
 );
-
-// If you want your app to work offline and load faster, you can change unregister() to register()
-// below. Note this comes with some pitfalls. Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
